765a2851db1e221b5241dafa0e4b553fc3778320
Doychin Atanasov <ironsmile@gmail.com>
2021-06-07 23:55:46 +0300

    Add a script for converting images

---
+++ _scripts/convert-images.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p "${1}/out"
+
+convertImage () {
+    dname=$(dirname "${1}")
+    fname=$(basename --suffix .jpg "${1}")
+    convert "${1}" \
+            -resize 2016x \
+            -sampling-factor 4:2:0 \
+            -strip \
+            -quality 85 \
+            -interlace JPEG \
+            -colorspace sRGB \
+            -rotate 90 \
+            "${dname}/out/${fname}.jpeg"
+}
+
+createThumbnail () {
+    dname=$(dirname "${1}")
+    fname=$(basename --suffix .jpg "${1}")
+    convert "${1}" \
+            -resize 403x \
+            -sampling-factor 4:2:0 \
+            -strip \
+            -quality 85 \
+            -interlace JPEG \
+            -colorspace sRGB \
+            -rotate 90 \
+            "${dname}/out/${fname}-thumb.jpeg"
+}
+
+export -f convertImage
+export -f createThumbnail
+
+find "$1" -maxdepth 1 -type f -iname '*.jpg' -exec \
+    bash -c 'convertImage "$0"' {} \;
+
+find "$1" -maxdepth 1 -type f -iname '*.jpg' -exec \
+    bash -c 'createThumbnail "$0"' {} \;
